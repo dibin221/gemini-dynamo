db6e2ae151d1d85cf044fc106faf3698f0d84f1a
Dibin Peter <dibin.peter@versed.ai>
2024-05-06 03:44:35 +0100

    Added backed files

---
+++ backend/constants.py
@@ -0,0 +1,7 @@
+from enum import Enum
+class VertexAIConfig(Enum):
+    LLM_CONFIG =  {
+        "model_name": "gemini-pro",
+        "project": "quizify-418619",
+        "location": "us-central1"
+    }
